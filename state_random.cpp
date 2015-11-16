35583dcce122954b5683b1bcf897695b7f6447ac
Paul Walger <metaxy@walger.name>
2015-11-16 12:14:04 +0100

    new random check if we modified this edge before

--- state_random.cpp
+++ state_random.cpp
@@ -1,16 +1,37 @@
 #include "state_random.h"
 #include "vf.h"
 #include "randomize.h"
+#include <unordered_map>
 StateRandom::StateRandom(Config conf) : State(conf), m_countSteps(0)
 {
 }
 
 MGraph StateRandom::solveSingle(MGraph input, MGraph forbidden)
 {
+    unordered_map<edge, int> modified;
+    vector<Edge> forbiddenEdges = forbidden.edges();
+    double forbiddenEdgesSize = forbiddenEdges.size();
+
     NodeMapping mapping = VF::subgraphIsoOne(&input, &forbidden);
     while(!mapping.empty()) {
-        Edge e = Common::transformEdge(r->randomElement(forbidden.edges()), &mapping);
-        input.flip(e);
+        Edge foundEdge;
+        while(true) {
+            Edge e = Common::transformEdge(r->randomElement(forbiddenEdges), &mapping);
+            bool mod = modified.find(e) != modified.end();
+            double prob = 0.0;
+            if(mod) {
+                prob = 1.0/(forbiddenEdgesSize*4.0);
+            } else {
+                prob = 1.0/(forbiddenEdgesSize);
+            }
+            if(r->choice(prob)) {
+                foundEdge = e;
+                break;
+            }
+
+        }
+
+        input.flip(foundEdge);
         m_countSteps++;
         mapping = VF::subgraphIsoOne(&input, &forbidden);
     }
