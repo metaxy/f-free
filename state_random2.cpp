34f499d9fa606c579ce9e7daac13bcfc1c7b0fb3
Paul Walger <metaxy@walger.name>
2015-11-16 12:15:06 +0100

    random2 ist old random

--- state_random2.cpp
+++ state_random2.cpp
@@ -8,17 +8,16 @@ StateRandom2::StateRandom2(Config conf) : State(conf)
 
 MGraph StateRandom2::solveSingle(MGraph input, MGraph forbidden)
 {
-    int batch = getInt("random2_subgraph_batch", 100);
-
-    vector<NodeMapping> mappings = VF::subgraphIso(&input, &forbidden, batch);
-    int countSteps = 0;
-    while(!mappings.empty()) {
-        for(NodeMapping mapping : mappings) {
-            Edge e = Common::transformEdge(r->randomElement(forbidden.edges()), &mapping);
-            input.flip(e);
-            countSteps++;
-        }
-        mappings = VF::subgraphIso(&input, &forbidden, batch);
+    NodeMapping mapping = VF::subgraphIsoOne(&input, &forbidden);
+    while(!mapping.empty()) {
+        Edge e = Common::transformEdge(r->randomElement(forbidden.edges()), &mapping);
+        input.flip(e);
+        m_countSteps++;
+        mapping = VF::subgraphIsoOne(&input, &forbidden);
     }
     return input;
+}
+void StateRandom2::final()
+{
+    m_debug["countSteps"] = std::to_string(m_countSteps);
 }
