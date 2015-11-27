5dd24bd063304e9efe69d858bc15b9bfc9e8ebd2
Paul Walger <metaxy@walger.name>
2015-11-27 13:21:03 +0100

    More threads

--- state_blp.cpp
+++ state_blp.cpp
@@ -6,42 +6,49 @@
 StateBlp::StateBlp(Config conf) : State(conf)
 {
 }
-MGraph StateBlp::solve()
+/*MGraph StateBlp::solve()
 {
     MGraph input(m_input);
     for(MGraph needle : m_forbidden) {
         input = this->solveSingle(input, needle);
     }
     return input;
-}
+}*/
 
-MGraph StateBlp::solveSingle(MGraph input, MGraph forbidden)
+MGraph StateBlp::solve()
 {
-    Randomize r(getInt("seed", 5489));
+    MGraph input(m_input);
     GurobiLP g(input.nodeCount());
     Model model = input.createModel();
     g.addModelVars(model);
     g.setObjective(model);
 
-    vector<NodeMapping> mappings = VF::subgraphIsoAll(&input, &forbidden);
-    int step = 0;
-    while(!mappings.empty()) {
-        clog << "[" << step << "] found isomorphisms " << mappings.size() << endl;
-        for(NodeMapping mapping : mappings) {
-            g.addConstraint(&input, &forbidden, &mapping);
-        }
-       
-        Model ret = g.optimize();
-        for(const auto &i: ret) {
-            if(i.second > 0) {
-                input.setWeight(i.first, 1);
-            } else {
-                input.setWeight(i.first, -1);
+    for(MGraph forbidden : m_forbidden) {
+        vector<NodeMapping> mappings = VF::subgraphIsoAll(&input, &forbidden);
+        int step = 0;
+        while(!mappings.empty()) {
+            clog << "[" << step << "] found isomorphisms " << mappings.size() << endl;
+            for(NodeMapping mapping : mappings) {
+                g.addConstraint(&input, &forbidden, &mapping);
             }
+
+            Model ret = g.optimize();
+            for(const auto &i: ret) {
+                if(i.second > 0) {
+                    input.setWeight(i.first, 1);
+                } else {
+                    input.setWeight(i.first, -1);
+                }
+            }
+            //copy.printEdges(copy.difference(&input));
+            step++;
+            mappings = VF::subgraphIsoAll(&input, &forbidden);
         }
-        //copy.printEdges(copy.difference(&input));
-        step++;
-        mappings = VF::subgraphIsoAll(&input, &forbidden);
     }
     return input;
+}
+
+MGraph StateBlp::solveSingle(MGraph input, MGraph forbidden)
+{
+
 }
