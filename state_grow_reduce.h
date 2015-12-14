83bafee69727fc8281cd49831529144d893ff52a
Paul Walger <metaxy@walger.name>
2015-12-14 11:14:52 +0100

    new algo: grow reduce

---
+++ state_grow_reduce.h
@@ -0,0 +1,21 @@
+#ifndef STATE_GROW_REDUCE_H
+#define STATE_GROW_REDUCE_H
+#include "state.h"
+class StateGrowReduce : public State
+{
+public:
+    StateGrowReduce(Config conf);
+    MGraph solve();
+    void final();
+
+    void grow(MGraph *graph);
+    void reduce(MGraph *graph);
+protected:
+    bool isValid(MGraph *input);
+    int m_countIteration;
+    int m_validChanges;
+    int m_invalidChanges;
+    int m_skipBecauseOfWeight;
+};
+
+#endif // STATE_GROW_REDUCE_H
