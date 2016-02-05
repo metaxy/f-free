c0c02a89467882998460178f13d5710f65e573d6
Paul Walger <metaxy@walger.name>
2016-02-05 17:04:13 +0100

    best script

---
+++ best.rb
@@ -0,0 +1,48 @@
+require 'date'
+require 'json'
+require 'fileutils'
+require 'optparse'
+$BIN_PATH = "./build"
+
+def main()
+  tools = ["extend", "grow_reduce3", "bottom", "random"];
+
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = "Usage: best.rb [options]"
+
+    opts.on("-i", "--input INPUT", "Which graph do you want to test?") do |v|
+      options[:input] = v
+    end
+  end.parse!
+  abort("no input") if options[:input].nil?
+  results = {}
+  Dir.entries("forbidden/").each do |forbidden|
+    next if forbidden.start_with? "."
+    puts "Testing #{forbidden}"
+    tools.each do |prog|
+      k = run(options[:input], "forbidden/"+forbidden, prog, "3")
+      if(k >= 0) 
+        if(results.has_key? forbidden)
+          if(results[forbidden] > k)
+            results[forbidden] = k
+          end
+        else
+          results[forbidden] = k
+        end
+      end  
+      
+    end
+  end
+  results.sort_by{|k,v| v}.to_h.each do |k,v|
+    puts "#{v}   #{k}"  
+  end
+end 
+
+def run(input, forbidden, prog, timeout)
+  ret = `timeout #{timeout}s #{$BIN_PATH}/ffree_#{prog} --input '#{input}' --forbidden '#{forbidden}'`
+  return -1 if(ret.chomp == "") 
+  return ret.split("\n").select{ |line| ! line.start_with?("#")}.size
+end
+
+main();
