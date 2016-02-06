1c5c386ff02808130cc9abd3b3deb0035a4e49c7
Paul Walger <metaxy@walger.name>
2016-02-06 12:37:03 +0100

    add results

---
+++ tests.sh
@@ -0,0 +1,33 @@
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/x172_triangle -p blp -t 300
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/p5_triangle -p blp -t 300
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/p4_c4 -p blp -t 300
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/p5_c5 -p blp -t 300
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/k14_paw -p blp -t 300
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/c4 -p blp -t 300
+ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/2k2_c4_c5 -p blp -t 300
+
+ruby generate_k.rb -i model/bio1/ -f forbidden/x172_triangle -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/p5_triangle -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/p4_c4 -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/p5_c5 -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/k14_paw -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/c4 -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/2k2_c4_c5 -p blp -t 300
+
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/x172_triangle -p blp -t 300
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/p5_triangle -p blp -t 300
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/p4_c4 -p blp -t 300
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/p5_c5 -p blp -t 300
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/k14_paw -p blp -t 300
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/c4 -p blp -t 300
+ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/2k2_c4_c5 -p blp -t 300
+
+
+ruby generate_k.rb -i model/bio2/ -f forbidden/x172_triangle -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/p5_triangle -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/p4_c4 -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/p5_c5 -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/k14_paw -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/c4 -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/2k2_c4_c5 -p blp -t 300
+
