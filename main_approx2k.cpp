f1eb4fef693a3d0260f57d594e62fa7c66ae5fcc
Paul Walger <metaxy@walger.name>
2016-02-10 12:04:20 +0100

    restore

--- main_approx2k.cpp
+++ main_approx2k.cpp
@@ -9,9 +9,15 @@ int main(int argc, char* argv[])
     vector<string> options = {"input", "forbidden", "rounds", "seed"};
     Config conf = Common::parseConfigOptions(argc, argv, options);
     StateApprox2K state;
-    GGraph i = Common::graphFromFile(conf["input"]);
+    string fileName = conf["input"];
+    if(fileName.empty()) {
+        fileName = "../model/albert_barabasi/n_120_m_3.txt";
+    }
+    GGraph i = Common::graphFromFile(fileName);
     MergeGraph input(i);
     MergeGraph solved = state.solve(input);
+    solved.restoreMerges();
+    solved.normalize();
     for(const Edge &e: input.difference(&solved)) {
         cout << i.nodeName(e.first) << " " << i.nodeName(e.second) << endl;
     }
