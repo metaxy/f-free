e184a7cf6cc5bc9930201b33ff8ff1cbe610967e
Paul Walger <metaxy@walger.name>
2016-02-17 21:32:40 +0100

    new results

--- tests.sh
+++ tests.sh
@@ -1,33 +1,41 @@
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/x172_triangle -p blp -t 300
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/p5_triangle -p blp -t 300
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/p4_c4 -p blp -t 300
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/p5_c5 -p blp -t 300
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/k14_paw -p blp -t 300
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/c4 -p blp -t 300
-ruby generate_k.rb -i model/albert_barabasi/ -f forbidden/2k2_c4_c5 -p blp -t 300
+TIME=500
 
-ruby generate_k.rb -i model/bio1/ -f forbidden/x172_triangle -p blp -t 300
-ruby generate_k.rb -i model/bio1/ -f forbidden/p5_triangle -p blp -t 300
-ruby generate_k.rb -i model/bio1/ -f forbidden/p4_c4 -p blp -t 300
-ruby generate_k.rb -i model/bio1/ -f forbidden/p5_c5 -p blp -t 300
-ruby generate_k.rb -i model/bio1/ -f forbidden/k14_paw -p blp -t 300
-ruby generate_k.rb -i model/bio1/ -f forbidden/c4 -p blp -t 300
-ruby generate_k.rb -i model/bio1/ -f forbidden/2k2_c4_c5 -p blp -t 300
-
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/x172_triangle -p blp -t 300
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/p5_triangle -p blp -t 300
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/p4_c4 -p blp -t 300
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/p5_c5 -p blp -t 300
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/k14_paw -p blp -t 300
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/c4 -p blp -t 300
-ruby generate_k.rb -i model/newman_watts_strogatz/ -f forbidden/2k2_c4_c5 -p blp -t 300
+ruby generate_k.rb -i model/bio1/ -f forbidden/2k2_c4_c5 -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/2p3 -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/3k2_triangle -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/c4 -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/c4_p5 -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/claw -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/cluster -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/cograph -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/k14_paw -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/p4_c4 -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/p5_c5 -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/p5_triangle -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/paw -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/split -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/splitcluster -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/triangle -p blp -t $TIME
+ruby generate_k.rb -i model/bio1/ -f forbidden/x172_triangle -p blp -t $TIME
 
 
-ruby generate_k.rb -i model/bio2/ -f forbidden/x172_triangle -p blp -t 300
-ruby generate_k.rb -i model/bio2/ -f forbidden/p5_triangle -p blp -t 300
-ruby generate_k.rb -i model/bio2/ -f forbidden/p4_c4 -p blp -t 300
-ruby generate_k.rb -i model/bio2/ -f forbidden/p5_c5 -p blp -t 300
-ruby generate_k.rb -i model/bio2/ -f forbidden/k14_paw -p blp -t 300
-ruby generate_k.rb -i model/bio2/ -f forbidden/c4 -p blp -t 300
-ruby generate_k.rb -i model/bio2/ -f forbidden/2k2_c4_c5 -p blp -t 300
+ruby generate_k.rb -i model/bio2/ -f forbidden/2k2_c4_c5 -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/2p3 -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/3k2_triangle -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/c4 -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/c4_p5 -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/claw -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/cluster -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/cograph -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/k14_paw -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/p4_c4 -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/p5_c5 -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/p5_triangle -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/paw -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/split -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/splitcluster -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/triangle -p blp -t $TIME
+ruby generate_k.rb -i model/bio2/ -f forbidden/x172_triangle -p blp -t $TIME
+
+
 
