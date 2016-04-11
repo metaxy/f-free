a12b721087027fb7a10c6515b09763f34b74104a
Paul Walger <metaxy@walger.name>
2016-04-11 12:26:27 +0200

    try with building

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@ cd src/
 wget http://netix.dl.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2
 tar xvjf boost_1_55_0.tar.bz2
 rm boost_1_55_0.tar.bz2
-cd ../
+cd boost_1_55_0/
+./boostrap.sh
+./b2
+cd ../../
 mkdir build
 cd build 
 cmake -DCMAKE_BUILD_TYPE="Release" ..
