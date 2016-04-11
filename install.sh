80632e99b0d6303a2896d7bd4180ef4d9a8f4468
Paul Walger <metaxy@walger.name>
2016-04-11 12:08:13 +0200

    install using install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
- 
+rm src/boost/ -rf
+cd src/
+wget http://netix.dl.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.bz2
+tar xvjf boost_1_60_0.tar.bz2
+rm boost_1_60_0.tar.bz2
+cd ../
+mkdir build
+cd build 
+cmake -DCMAKE_BUILD_TYPE="Release" ..
+make
