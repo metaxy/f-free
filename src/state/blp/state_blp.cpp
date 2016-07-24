8bde666980d0dea4a131f2497fcb3493ad1b43db
Paul Walger <metaxy@walger.name>
2016-07-24 17:38:49 +0200

    fehler: bip könnte nicht korrekte lösung zurückgeben

--- src/state/blp/state_blp.cpp
+++ src/state/blp/state_blp.cpp
@@ -12,24 +12,26 @@ MGraph StateBlp::solve()
     Model model = input.createModel();
     g.addModelVars(model);
     g.setObjective(model);
-    for(const MGraph &needle : m_forbidden) {
-        vector<NodeMapping> mappings = VF::subgraphIsoAll(&input, &needle);
-        int step = 0;
-        while(!mappings.empty()) {
-            for(NodeMapping mapping : mappings) {
-                g.addConstraint(&input, &needle, &mapping);
-            }
-
-            Model ret = g.optimize();
-            for(const auto &i: ret) {
-                if(i.second > 0) {
-                    input.setConnected(i.first, true);
-                } else {
-                    input.setConnected(i.first, false);
+    while(!isValid(&input)) {
+        for(const MGraph &needle : m_forbidden) {
+            vector<NodeMapping> mappings = VF::subgraphIsoAll(&input, &needle);
+            int step = 0;
+            while(!mappings.empty()) {
+                for(NodeMapping mapping : mappings) {
+                    g.addConstraint(&input, &needle, &mapping);
                 }
+
+                Model ret = g.optimize();
+                for(const auto &i: ret) {
+                    if(i.second > 0) {
+                        input.setConnected(i.first, true);
+                    } else {
+                        input.setConnected(i.first, false);
+                    }
+                }
+                step++;
+                mappings = VF::subgraphIsoAll(&input, &needle);
             }
-            step++;
-            mappings = VF::subgraphIsoAll(&input, &needle);
         }
     }
     return input;
